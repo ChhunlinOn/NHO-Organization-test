75fa17e14cda73cdb725a31b6cefc9699235e0cc
Rotanakbot Bunthoeun <rotanakbotbunthoeun@Mac.lan>
2025-08-14 17:40:46 +0700

    finished page board director

---
+++ src/components/ui/boarddirector/card.tsx
@@ -0,0 +1,22 @@
+// // components/ui/card.tsx
+// import React from "react"
+
+// interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+//   children: React.ReactNode
+// }
+
+// export const Card = ({ children, className = "", ...props }: CardProps) => {
+//   return (
+//     <div className={`bg-white shadow-md rounded-xl p-4 ${className}`} {...props}>
+//       {children}
+//     </div>
+//   )
+// }
+
+// export const CardContent = ({ children, className = "", ...props }: CardProps) => {
+//   return (
+//     <div className={`p-2 ${className}`} {...props}>
+//       {children}
+//     </div>
+//   )
+// }
